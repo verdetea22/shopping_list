19fecd8f0d3c4cabd0200786b060f4a5e94b2012
mmJDV <mmount@jdvproducts.com>
2024-10-25 10:09:49 -0400

    form validation

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -10,7 +10,6 @@ class NewItem extends StatefulWidget {
   }
 }
 class _NewItemState extends State<NewItem> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,10 +27,17 @@ class _NewItemState extends State<NewItem> {
                   label: Text('Name'),
                 ),
                 validator: (value) {
-                  return 'demo'; //error message
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters.';
+                  }
+                  return null;
                 },
               ), //instead of TextField()
-              Row(
+               Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Expanded(
                     child: TextFormField(
@@ -39,6 +45,15 @@ class _NewItemState extends State<NewItem> {
                         label: Text('Quantity'),
                       ),
                       initialValue: '1',
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid, positive number.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(width: 8),
@@ -62,7 +77,6 @@ class _NewItemState extends State<NewItem> {
                           ),
                       ], 
                       onChanged: (value) {
-                        
                       },
                     ),
                   )
