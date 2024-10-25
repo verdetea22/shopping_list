72afba119be2aa483406b4c5c260f01dcce342b8
mmJDV <mmount@jdvproducts.com>
2024-10-25 10:45:06 -0400

    reset w formkey

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -94,7 +94,9 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
                     child: const Text('Reset'),
                   ),
                   ElevatedButton(
