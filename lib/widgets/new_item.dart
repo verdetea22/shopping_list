d86a6b7fd82f7843c57916d7853287160f8c5100
mmJDV <mmount@jdvproducts.com>
2024-10-25 10:39:39 -0400

    add item

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -10,6 +10,12 @@ class NewItem extends StatefulWidget {
   }
 }
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveItem() {
+    _formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +25,7 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
@@ -82,6 +89,20 @@ class _NewItemState extends State<NewItem> {
                   )
                 ],
               ),
+               const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _saveItem,
+                    child: const Text('Add Item'),
+                  )
+                ],
+              ),
             ],
           ),
         ),
@@ -89,5 +110,3 @@ class _NewItemState extends State<NewItem> {
     );
   }
 }
-
-
