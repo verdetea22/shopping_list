33d80c29103900ad19d3cba43100b3fea282cc28
mmJDV <mmount@jdvproducts.com>
2024-10-25 13:05:30 -0400

    http works with firebase

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
@@ -21,13 +26,19 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(), 
-          name: _enteredName, 
-          quantity: _enteredQuantity, 
-          category: _selectedCategory),
-        );
+      final url = Uri.https('flutter-shopping-list-1194c-default-rtdb.firebaseio.com', 'shopping-list.json');
+      http.post(url,headers: {
+        'Content-Type': 'application/json',
+      }, 
+      body: json.encode(
+          {
+            'name': _enteredName, 
+            'quantity': _enteredQuantity, 
+            'category': _selectedCategory.title,
+          },
+        ),s
+      );
+      // Navigator.of(context).pop();
     }
   }
 
